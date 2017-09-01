e82558e2853d4746f67da88b0af5791e1837228d
Adrian Rupala <adrixop95@me.com>
2017-09-02 01:35:25 +0200

    nareszcie dziala

--- Java apps/NetBeansProjects/pokaz/src/JavaDB_001/Pokaz.java
+++ Java apps/NetBeansProjects/pokaz/src/JavaDB_001/Pokaz.java
@@ -1,17 +1,17 @@
 package JavaDB_001;
 
 import java.awt.Color;
-import java.awt.Desktop;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.ByteArrayOutputStream;
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
-
-
 
 
 public class Pokaz extends JFrame{
@@ -20,21 +20,10 @@ public class Pokaz extends JFrame{
     int x = 0;
     public static int y = 0;
     
-    //Images Path In Array
-    /*String[] list = {
-                      "/Users/adrix/Desktop/pics/1.jpg",//0
-                      "/Users/adrix/Desktop/pics/2.jpg",//1
-                      "/Users/adrix/Desktop/pics/3.mp4",
-                    };
-    */
-    
-    ArrayList<String> list = new ArrayList<String>(){{
-        add("/Users/adrix/Desktop/pics/1.jpg");
-        add("/Users/adrix/Desktop/pics/2.jpg");
-    }};
+    public static ArrayList<String> list = new ArrayList<String>();
     
     public Pokaz(){
-        super("Pokaz SMCEBI");
+        super("Pokaz_SMCEBI");
         pic = new JLabel();
         pic.setBounds(0, 0, 1366, 768);
 
@@ -72,18 +61,23 @@ public class Pokaz extends JFrame{
         pic.setIcon(newImc);
     }
 
-    public static void main(String[] args){ 
+    public static void main(String[] args) throws FileNotFoundException{ 
         
     File path = new File("/Users/adrix/Desktop/pics/");
 
     File [] files = path.listFiles();
+    
     for (int i = 0; i < files.length; i++){
         if (files[i].isFile()){ //this line weeds out other directories/folders
             System.out.println(files[i]);
             y = y+1-1;
+            ByteArrayOutputStream test = new ByteArrayOutputStream();
+            PrintStream PS = new PrintStream(test);
+            PrintStream old = System.out;
+            System.setOut(PS);
+            list.add(files[i].toString());
         }
     }
-      
         new Pokaz();
     }
 }
