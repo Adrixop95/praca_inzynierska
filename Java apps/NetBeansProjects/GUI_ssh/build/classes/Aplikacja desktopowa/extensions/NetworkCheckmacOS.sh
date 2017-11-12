69d7fcad32239960cf64cf154338e9fa8c5326e9
Adrixop95 <adrixop95@me.com>
2017-11-12 20:19:22 +0100

    small cleanup

--- Java apps/NetBeansProjects/GUI_ssh/build/classes/Aplikacja desktopowa/extensions/NetworkCheckmacOS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Created by Adrian Rupala 2017
-#Simple bash script for searching raspberry pi on network with arp and nmap command.
-
-printf '\nTwoje Raspberry PI w sieci: \n\n'
-arp -a | grep 'b8:27:eb'
-printf '\n'
