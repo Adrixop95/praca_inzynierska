fcf81479bb81254bec25e6dadd0f0c97dd97a433
Adrixop95 <adrixop95@me.com>
2018-02-01 19:53:56 +0100

    komentarze

--- Bash scripts/avilible_networks.sh
+++ Bash scripts/avilible_networks.sh
@@ -3,5 +3,9 @@
 #Created by Adrian Rupala
 #Simple script to show available wireless network
 
-#Wykonanie komendy iw odpowiadającej za połączenia bezprzewodowe oraz przesortowanie po nazwie SSID
-sudo iw dev wlan0 scan | grep SSID
+#Pobranie z pliku /cat/net/wireless oraz odpowiednie przesortowanie wyniku informacji o kartcie sieciowej
+SUBSTRING=$(cat /proc/net/wireless | grep : | cut -d' ' -f 2)
+net="${SUBSTRING::-1}"
+#Przjście do folderu z aplikacjami oraz uruchomienie aplikacji iwlist odpowiedzialnej
+# między innymi za wyszukiwanie dostępnej sieci wifi a następnie przesortowanie wyniku w celu odpowiedniego dopasowania go do aplikacji
+cd /sbin/ && ./iwlist $net scan | grep ESSID | cut -d':' -f 2 | tr -d '"' | sed 's/.*/: &/'
--- Bash scripts/smcebi_avilible_networks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Created by Adrian Rupala
-#Simple script to show available wireless network
-
-SUBSTRING=$(cat /proc/net/wireless | grep : | cut -d' ' -f 2)
-net="${SUBSTRING::-1}"
-prefix=": "
-cd /sbin/ && ./iwlist $net scan | grep ESSID | cut -d':' -f 2 | tr -d '"' | sed 's/.*/: &/'
